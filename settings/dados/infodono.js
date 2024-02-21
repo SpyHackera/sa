49a824d7e1054b1aa5063d3a00091293a265df80
SpyHackera <156230895+SpyHackera@users.noreply.github.com>
2024-02-21 00:13:44 -0300

    Update infodono.js

--- settings/dados/infodono.js
+++ settings/dados/infodono.js
@@ -4,8 +4,8 @@ const infodono = (prefix, numerodn, NomeDoBot, sender) => {
 // NÃO APAGUE ESSE ${NickDono} nem 
 //${numerodn} nem ${NomeDoBot} nem ${prefix} só se quiser apagar completo, não coloque nada ${dentro assim} ISSO SÃO DEFINIÇÕES QUE ESTÁ PUXANDO DO settings.json, da pasta dono, só pode altera a base de tudo, menos as definições, só se quiser apagar a definição completa.
 
-return `Olá, usuário: @${sender.split("@")[0]} !
-Número do meu proprietário: wa.me/${numerodn}`
+return `Salve, usuário: @${sender.split("@")[0]} !
+Número do meu @PapaiSpy: wa.me/554884702848`
 }
 
 exports.infodono = infodono
